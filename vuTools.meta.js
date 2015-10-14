7c581a88527dfe1fb5777fae978cb6c76019b9b9
Ivan Jelenić <ivan.jelenic42@gmail.com>
2015-10-14 22:32:29 +0200

    Added update check path + file, updated version

---
+++ vuTools.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         vuTools
+// @version      0.21
+// @author       Ivan Jelenić (Quirinus)
+// @description  A userscript to improve various user interface bits of the Visual Utopia browser game.
+// @homepage     https://github.com/Quirinus/
+// @namespace    https://github.com/Quirinus/
+// @downloadURL  https://raw.githubusercontent.com/Quirinus/vuTools/master/vuTools.user.js
+// @updateURL    https://raw.githubusercontent.com/Quirinus/vuTools/master/vuTools.meta.js
+// @supportURL   https://github.com/Quirinus/vuTools/issues
+// @icon         http://visual-utopia.com/favicon.ico
+// @match        http://visual-utopia.com/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
+// @grant        none
+// ==/UserScript==
